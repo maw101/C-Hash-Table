13bb595d138b36e9bfc02f7fe32292d8fc20fd7c
maw101 <48567180+maw101@users.noreply.github.com>
2023-01-27 11:34:57 +0000

    Defined table initialisation function

--- src/hash_table.c
+++ src/hash_table.c
@@ -12,3 +12,14 @@ static hash_table_item * hash_table_new_item(const char * k, const char * v) {
 
     return item;
 }
+
+// table initialisation function
+hash_table_table * hash_table_new() {
+    hash_table_table * hash_table = malloc(sizeof(hash_table_table)); // TODO: check malloc response
+
+    hash_table->count = 0;
+    hash_table->items = calloc((size_t) hash_table->size, sizeof(hash_table_item *)); // TODO: check calloc response
+    hash_table->size = 50; // TODO: add resize ability in future
+
+    return hash_table;
+}
