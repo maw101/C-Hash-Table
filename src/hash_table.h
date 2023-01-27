8685540d4c572f7045d6ba328d525f604003e6f4
maw101 <48567180+maw101@users.noreply.github.com>
2023-01-27 11:24:41 +0000

    Defined item struct for K-V pairs

--- src/hash_table.h
+++ src/hash_table.h
@@ -1,4 +1,10 @@
 #ifndef SRC_HASH_TABLE_H
 #define SRC_HASH_TABLE_H
 
+// item struct for key-value pairs
+typedef struct {
+    char * key;
+    char * value;
+} hash_table_item;
+
 #endif //SRC_HASH_TABLE_H
