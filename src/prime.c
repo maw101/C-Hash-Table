98a74cf4ff69a454dd308ea33498840906b5b434
maw101 <48567180+maw101@users.noreply.github.com>
2023-02-07 11:26:17 +0000

    #4 - Prime support

--- src/prime.c
+++ src/prime.c
@@ -1,1 +1,29 @@
+#include <math.h>
+
 #include "prime.h"
+
+int is_prime(const int x) {
+    if (x < 2) {
+        return -1;
+    } else if (x < 4) {
+        return 1;
+    } else if ((x % 2) == 0) {
+        return 0;
+    } else {
+        for (int i = 3; i <= floor(sqrt((double) x)); i += 2) {
+            if ((x % i) == 0) {
+                return 0;
+            }
+        }
+
+        return 1;
+    }
+}
+
+int get_next_prime(int x) {
+    while (is_prime(x) != 1) {
+        x++;
+    }
+
+    return x;
+}
